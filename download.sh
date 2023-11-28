41d1b8acbd90b53cb8444446ae554fbb5f481079
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-11-28 15:39:00 -0500

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,9 @@
+gdown https://drive.google.com/uc?id=1LXTVIG09Uu__0FgPq_AUT682haDdLYtq
+unzip test_data.zip
+gdown https://drive.google.com/uc?id=1Amvjt6e5mhHI5wndWkr5UrX6Dq_oikEm
+unzip exp_rir.zip
+mv exp RIR_Estimation
+gdown https://drive.google.com/uc?id=1ZQDo3Rxlk702-iMXeG8QySfr3Mr3Es44
+unzip exp_en.zip
+mv exp Enhancement
+rm -rf *.zip
